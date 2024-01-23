000990d9f9b570ffbbe1401db3966b4c44e7306c
EFFEXDEE1 <156126574+EFFEXDEE1@users.noreply.github.com>
2024-01-23 18:10:51 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 createApp(App).mount('#app')
